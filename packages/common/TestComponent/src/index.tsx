b75d9fe14b9f3e6a52dbceb8ca2bccd63ab522b0
Austin Peterson <austin@akpwebdesign.com>
2020-02-29 17:40:11 -0800

    make repro more minimal

---
+++ packages/common/TestComponent/src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const TestComponent: React.FC = () => (
+  <div>test component or something</div>
+);
+
+export default TestComponent;
---
+++ packages/web/Web/src/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestComponent from '@whenapp/component-test';
+
+ReactDOM.render(<TestComponent />, document.getElementById('app'));
